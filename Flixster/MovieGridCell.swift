ad1900007b0af87f12600036bd84988ba1ac22e7
atakahashi19 <39812344+atakahashi19@users.noreply.github.com>
2020-10-02 21:20:31 -0700

    unit 2: superhero tab added

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Amanda Takahashi on 10/2/20.
+//  Copyright © 2020 amanda.takahashi. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
